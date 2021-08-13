0fbb226956dfbfc1dd948d94aefe73d5d731f62c
Rupesh Budhathoki <54392112+Roopaish@users.noreply.github.com>
2021-08-13 21:44:03 +0545

    portfolio1

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
 module.exports = function (eleventyConfig) {
-
-  eleventyConfig.addPassthroughCopy('./src/style.css');
-  eleventyConfig.addPassthroughCopy('./src/assets');
+  eleventyConfig.addPassthroughCopy("./src/style.css")
+  eleventyConfig.addPassthroughCopy("./src/portfolio.css")
+  eleventyConfig.addPassthroughCopy("./src/assets")
 
   return {
     dir: {
